96e4eb5a1a82ad8537fdeb96a73a8255fed42f1a
_bidhayak <rishikeyyadav@gmail.com>
2024-09-29 21:11:34 +0530

    working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{any::{type_name, type_name_of_val}, io, string};
-use reqwest::Client;
+
+use reqwest::blocking::{Client, Response}; 
 use std::collections::HashMap;
 
 fn type_of<T>(_: T) -> &'static str {
@@ -8,28 +9,35 @@ fn type_of<T>(_: T) -> &'static str {
 
 
 
- fn api_call(client: &Client,inp: String){
+ fn api_call(client: &Client,inp: String) -> Result<Response, reqwest::Error>{   
 
-let url="GET /current.json?q=53.1%2C-0.13 HTTP/1.1
-X-Rapidapi-Key: 1db8c01401msha79269e2ab23f60p1ad899jsndcbac7f7c6f2
-X-Rapidapi-Host: weatherapi-com.p.rapidapi.com
-Host: weatherapi-com.p.rapidapi.com
-";
+// let url="GET /current.json?q=53.1%2C-0.13 HTTP/1.1
+// X-Rapidapi-Key: 1db8c01401msha79269e2ab23f60p1ad899jsndcbac7f7c6f2
+// X-Rapidapi-Host: weatherapi-com.p.rapidapi.com
+// Host: weatherapi-com.p.rapidapi.com
+// ";
+
+let url = "https://weatherapi-com.p.rapidapi.com/current.json";
 
 let mut params=HashMap::new();
 params.insert("q",inp);
-let res = client.post(url)
-    .query(&params)
-    .send()
+let res = client
+   .post(url)
+   .header("X-Rapidapi-Key", "1db8c01401msha79269e2ab23f60p1ad899jsndcbac7f7c6f2")
+   .header("X-Rapidapi-Host", "weatherapi-com.p.rapidapi.com")
+   .query(&params)
+   .send()?;
+   //  .text()
+   //  .expect("Reason??");
     
 
-   return res;
+   return  Ok((res));
     
 }
 
 fn main() {
     let mut s=String::new();
-    let  client = reqwest::Client::new();
+    let  client = reqwest::blocking::Client::new();
 
      loop {
         println!("Enter the city or ZIP to get Weather details");
@@ -43,10 +51,31 @@ fn main() {
                println!("zip option\n");
 
                   //call weather api
-                  let resp=api_call(&client,inp);
-                  print!("{resp}");
+               //    let resp=api_call(&client,inp);
+               //    //let resp_body=resp;
 
-               continue;
+               //    print!("{}",resp);
+
+               // continue;
+
+               match api_call(&client, inp) {
+                  Ok(resp) => {
+                      // Handle the response body
+                      match resp.text() {
+                          Ok(resp_body) => {
+                              println!("Response Body: {}", resp_body);
+                          }
+                          Err(e) => {
+                              println!("Failed to read response body: {}", e);
+                          }
+                      }
+                  }
+                  Err(e) => {
+                      println!("API call failed: {}", e);
+                  }
+              }
+              continue;
+
             }
 
              }
